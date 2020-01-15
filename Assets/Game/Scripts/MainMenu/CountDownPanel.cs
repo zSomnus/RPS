0b4f955e708ad9a9802f4073e7a76fba978b5028
Xun <galaxymacos@gmail.com>
2020-01-14 19:20:35 -0500

    Transplant the project to 2019.2

---
+++ Assets/Game/Scripts/MainMenu/CountDownPanel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace MainGame.Scripts.MainMenu
+{
+    public class CountDownPanel : MonoBehaviour
+    {
+        public TextMeshProUGUI countDownText;
+
+        private float countDownTime;
+
+
+
+        private void OnEnable()
+        {
+            countDownTime = MainMenuManager.instance.countDownTime;
+
+        }
+
+        private void Update()
+        {
+            countDownTime -= Time.deltaTime;
+            if (countDownTime >= 0)
+            {
+                countDownText.text = $"Countdown: {(int) countDownTime}";
+            }
+        }
+    }
+}
